fa1ed5258917ac6d704b1ccf24efeb12e17a15eb
jheg <jason@jheg.co.uk>
2015-05-27 16:08:56 +0100

    added welcome page and all tests passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_team_members, :set_properties
+
+  private
+
+  def set_team_members
+    @team_members = TeamMember.all
+  end
+
+  def set_properties
+    @properties = Property.all
+  end
+
 end
