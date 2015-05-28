4affa73d8783ba129cd7636a2d042245225cd729
jheg <jason@jheg.co.uk>
2015-05-28 23:05:57 +0100

    added latest properties method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :set_team_members, :set_properties
+  before_action :set_team_members, :set_properties, :set_latest_properties
 
   private
 
@@ -12,6 +12,10 @@ class ApplicationController < ActionController::Base
 
   def set_properties
     @properties = Property.all
+  end
+
+  def set_latest_properties
+    @latest_properties = Property.where('created_at > ?', 1.days.ago)
   end
 
 end
